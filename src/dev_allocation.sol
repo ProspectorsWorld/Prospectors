430f5d40f7d2fd44943940a42d1a79ff28ef4194
ProspectorsWorld <support@prospectors.io>
2017-08-07 21:24:44 +0300

    Update dev_allocation.sol

--- src/dev_allocation.sol
+++ src/dev_allocation.sol
@@ -5,31 +5,41 @@ import "lib/owned.sol";
 contract ProspectorsDevAllocation is Owned
 {
     ProspectorsGoldToken public token;
-    uint private initial_time;
-    uint private constant start_amount = 50000000 * 10**18;
+    uint public initial_time;
+
+    mapping(uint => bool) public unlocked;
+    mapping(uint => uint) public unlock_times;
+    mapping(uint => uint) unlock_values;
     
-    function ProspectorsDevAllocation(address _owner)
+    //contract with PGL tokens for Prospectors developers. Tokens will be frozen up to 4 years
+    function ProspectorsDevAllocation(address _token)
     {
-        token = ProspectorsGoldToken(msg.sender);
-        owner = _owner;
+        token = ProspectorsGoldToken(_token);
+    }
+    
+    function init() onlyOwner
+    {
+        if (token.balanceOf(this) == 0 || initial_time != 0) revert();
         initial_time = block.timestamp;
+        uint unlock_amount = token.balanceOf(this) / 5; //one part - 20% of tokens
+
+        unlock_values[0] = unlock_amount;
+        unlock_values[1] = unlock_amount;
+        unlock_values[2] = unlock_amount;
+        unlock_values[3] = unlock_amount;
+        unlock_values[4] = unlock_amount;
+        
+        unlock_times[0] = 180 days; //20% of tokens will be available after 180 days
+        unlock_times[1] = 360 days; //20% of tokens will be available after 360 days
+        unlock_times[2] = 720 days; //20% of tokens will be available after 2 years
+        unlock_times[3] = 1080 days; //20% of tokens will be available after 3 years
+        unlock_times[4] = 1440 days; //20% of tokens will be available after 4 years
     }
-    
-    function unlock_first_part()
+
+    function unlock(uint part)
     {
-        if (token.balanceOf(this) < start_amount || block.timestamp < initial_time + 180 days) revert();
-        token.transfer(owner, 20000000 * 10**18);
-    }
-    
-    function unlock_last_part()
-    {
-        if (token.balanceOf(this) == 0 || block.timestamp < initial_time + 360 days) revert();
-        token.transfer(owner, token.balanceOf(this));
-    }
-    
-    function kill() onlyOwner
-    {
-        if (token.balanceOf(this) != 0) revert();
-        selfdestruct(owner);
+        if (unlocked[part] == true || block.timestamp < initial_time + unlock_times[part] || unlock_values[part] == 0) revert();
+        token.transfer(owner, unlock_values[part]);
+        unlocked[part] = true;
     }
 }
