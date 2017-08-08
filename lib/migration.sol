6ed8ee90f44af99de53bb7e672242a3bb6a9b7d0
ProspectorsWorld <support@prospectors.io>
2017-08-08 11:11:18 +0300

    Update migration.sol

--- lib/migration.sol
+++ lib/migration.sol
@@ -10,16 +10,22 @@ contract Migrable is TokenBase, Owned
 
 
     function migrate() external {
+        migrate_participant(msg.sender);
+    }
+    
+    function migrate_participant(address _participant) internal
+    {
         // Abort if not in Operational Migration state.
         if (migrationAgent == 0)  revert();
-        if (_balances[msg.sender] == 0)  revert();
+        if (_balances[_participant] == 0)  revert();
         
-        uint256 _value = _balances[msg.sender];
-        _balances[msg.sender] = 0;
+        uint256 _value = _balances[_participant];
+        _balances[_participant] = 0;
         _supply = sub(_supply, _value);
         totalMigrated = add(totalMigrated, _value);
-        MigrationAgent(migrationAgent).migrateFrom(msg.sender, _value);
-        Migrate(msg.sender, migrationAgent, _value);
+        MigrationAgent(migrationAgent).migrateFrom(_participant, _value);
+        Migrate(_participant, migrationAgent, _value);
+        
     }
 
     function setMigrationAgent(address _agent) onlyOwner external {
